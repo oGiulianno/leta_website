1722452095af9a2be721bea430176bdd61e710b8
Hermeson Freitas <42178454+leftbrain3d@users.noreply.github.com>
2022-09-07 00:51:29 -0300

    Update Menu.tsx

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -3,7 +3,23 @@ import Link from 'next/link'
 export function Menu() {
 	return (
 		<div>
+			<Link href='/'>
+				<a>
+					Sobre Nós
+				</a>
+			</Link>
 
+			<Link href='/'>
+				<a>
+					Pesquisas
+				</a>
+			</Link>
+
+			<Link href='/'>
+				<a>
+					Eventos
+				</a>
+			</Link>
 		</div>
 	)
 }
